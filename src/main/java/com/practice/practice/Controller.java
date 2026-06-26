6048f91ba7a6c57690c47cbcc9f96dae9eb3a697
funkywalker <mrtrokon@yahoo.com>
2026-06-26 17:33:43 -0400

    initial commit

---
+++ src/main/java/com/practice/practice/Controller.java
@@ -0,0 +1,13 @@
+package com.practice.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello FUnky Walker";
+    }
+}
